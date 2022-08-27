6a262108686b0e01f890c2fae068e74200df3655
Flavio José Suárez Carmona <fjosescarmona@gmail.com>
2022-08-26 19:42:39 -0500

    test: to test the first commit

---
+++ src/main/java/com/example/cbapiexample/CircuitbreakerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.cbapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitbreakerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitbreakerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cbapiexample/CircuitbreakerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cbapiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitbreakerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
